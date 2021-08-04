5ef7f5e72abb6f421e96fe9f6d5208612762afab
Kennedy-Mwaura <kennedy.mwaura@student.moringa.school.com>
2021-08-04 15:03:30 +0300

    added a navber and intro sections

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+
+// Material Select Initialization
+$(document).ready(function () {
+    // $('.mdb-select').material_select();
+    });
+
+    // SideNav Initialization
+    $(".button-collapse").sideNav();
+
+
+
