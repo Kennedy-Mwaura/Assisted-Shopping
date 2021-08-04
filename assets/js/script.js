ff923c436938b03429b771eb132c80a214795ff5
Ngundo Muithya <ngundolarrymuithya@gmail.com>
2021-08-04 13:40:46 +0300

    Introduce js code that gives the buttons their functionality.

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function () {
+    jQuery("button#learn-more").click(function () {
+        jQuery("#hidden-details").show();
+        jQuery("button#hide").show();
+        jQuery("button#learn-more").hide();
+    });
+    jQuery("button#hide").click(function () {
+        jQuery("#hidden-details").hide();
+        jQuery("button#learn-more").show();
+        jQuery("button#hide").hide();
+    });
+});
